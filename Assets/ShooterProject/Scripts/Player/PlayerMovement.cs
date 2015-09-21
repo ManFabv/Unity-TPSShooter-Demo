9ed8eef7ba73675f35da7d5097ef1686a64ef74a
Fabricio Manrique <fabemanrique@hotmail.com>
2015-09-21 13:40:55 -0300

    Con un nuevo joystick (CnControls) implementado

---
+++ Assets/ShooterProject/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerMovement : MonoBehaviour 
+{	
+	public float translationSpeed = 3.0f;
+	public bool invertAxis = false;
+	
+	private Vector3 movement;
+	
+	private Rigidbody playerRigidBody;
+	
+	void Start()
+	{
+		playerRigidBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(invertAxis)
+			movement = new Vector3(CnInputManager.GetAxis("Horizontal"), 0.0f, -CnInputManager.GetAxis("Vertical"));
+		else
+			movement = new Vector3(CnInputManager.GetAxis("Horizontal"), 0.0f, CnInputManager.GetAxis("Vertical"));
+		
+        playerRigidBody.velocity = movement * translationSpeed;
+	}
+}
