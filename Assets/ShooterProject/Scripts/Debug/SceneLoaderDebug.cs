ca0989e5f0e0e207331eb86b3ba9419f0ab5ed9e
Silvio Lobo <silviolobo47@gmail.com>
2015-09-24 17:21:41 -0300

    Creada una escena debug de menu donde elegir la escena a cargar.

---
+++ Assets/ShooterProject/Scripts/Debug/SceneLoaderDebug.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoaderDebug : MonoBehaviour {
+
+    public string nombreEscenaSilvio = "";
+    public string nombreEscenaFabricio = "";
+
+	public void CargarEscenaSilvio()
+    {
+        Application.LoadLevel(nombreEscenaSilvio);
+    }
+
+    public void CargarEscenaFabricio()
+    {
+        Application.LoadLevel(nombreEscenaFabricio);
+    }
+}
