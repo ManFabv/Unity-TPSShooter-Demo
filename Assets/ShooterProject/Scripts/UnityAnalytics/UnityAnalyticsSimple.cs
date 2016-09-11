6e148f2990737fe9752d039628329c8506317ddc
Fabricio Manrique <fabemanrique@hotmail.com>
2016-09-11 02:38:48 -0300

    Updated to Unity 5.4.1 Validated Unity Ads, Monetization, and User Settings

---
+++ Assets/ShooterProject/Scripts/UnityAnalytics/UnityAnalyticsSimple.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class UnityAnalyticsSimple : MonoBehaviour
+{
+
+    int totalPotions = 5;
+    int totalCoins = 100;
+    string weaponID = "Weapon_102";
+
+
+    void Start()
+    {
+        //TEST CUSTOM EVENT ANALYTICS
+        Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+          {
+            { "potions", totalPotions
+        },
+            { "coins", totalCoins },
+            { "activeWeapon", weaponID }
+          });
+
+        //TEST MONETIZATION
+        Analytics.Transaction("12345abcde", 0.99m, "USD", null, null);
+
+        //USER ATTRIBUTES
+        Gender gender = Gender.Female;
+        Analytics.SetUserGender(gender);
+
+        int birthYear = 2014;
+        Analytics.SetUserBirthYear(birthYear);
+    }
+}
