54c6cf83620748a8e444fcb9c03ce7dd3f46cb61
Fabricio Manrique <fabemanrique@hotmail.com>
2016-11-13 09:16:15 -0300

    Se agrego un GameManager que hace que se ignoren las colisiones entre las armas del jugador y el jugador. Ademas se creo una clase Literales que contiene todos los strings fijos que hagan falta

---
+++ Assets/ShooterProject/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Clase que me servira para ir manejando las diferentes cuestiones del juego
+/// </summary>
+public class GameManager : Singleton<GameManager>
+{
+    /// <summary>
+    /// Aqui guardare cual es el layer de las armas del jugador
+    /// para que no colisione con los colliders del jugador
+    /// </summary>
+    private LayerMask layerArmasJugador;
+
+    /// <summary>
+    /// Aqui guardare el layer del player para que no colisione consigo mismo
+    /// </summary>
+    private LayerMask layerPlayer;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        //obtengo el layer a traves del nombre
+        layerArmasJugador = LayerMask.NameToLayer(Literales.layerNamePlayerWeapon);
+
+        //obtengo el layer a traves del nombre
+        layerPlayer = LayerMask.NameToLayer(Literales.layerNamePlayer);
+
+        //Digo que ignore las colisiones entre las armas del jugador y el jugador
+        Physics.IgnoreLayerCollision(layerArmasJugador, layerPlayer);
+
+        //digo que ignore las colisiones entre todos los colliders del player
+        Physics.IgnoreLayerCollision(layerPlayer, layerPlayer);
+	}
+}
---
+++ Assets/ShooterProject/Scripts/Utils/Literales.cs
@@ -0,0 +1,12 @@
+﻿public class Literales : Singleton<Literales>
+{
+    /// <summary>
+    /// Referencia al layer de las armas del jugador
+    /// </summary>
+    public static readonly string layerNamePlayerWeapon = "CarriedWeapon";
+    
+    /// <summary>
+    /// Referencia al layer del jugador
+    /// </summary>
+    public static readonly string layerNamePlayer = "Player";
+}
